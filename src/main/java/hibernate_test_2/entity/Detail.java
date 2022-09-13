e3af56ca0a47734906b1107083de3830d458add3
Oleg Sazonov <sazonov1994@gmail.com>
2022-09-13 22:56:38 +0300

    Add Detail class and use "Foreign key" in Employee class Use One-to-one dependency (uni-directional)

---
+++ src/main/java/hibernate_test_2/entity/Detail.java
@@ -0,0 +1,34 @@
+package hibernate_test_2.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "details")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    public Detail(String city, String phoneNumber, String email) {
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
---
+++ src/main/java/hibernate_test_2/entity/Employee.java
@@ -0,0 +1,41 @@
+package hibernate_test_2.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "salary")
+    private int salary;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "details_id")
+    private Detail empDetail;
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+}
